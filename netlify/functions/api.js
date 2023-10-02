4d492b42b05b601564e0748ec08a66a70cb34383
tarawat <tarawat@paypal.com>
2023-10-02 12:10:10 +0530

    feat: v1

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,16 +4,22 @@ const express = require('express')
 
 const app = express();
 
-const router = express.Router();
-//router.use(express.static('../functions/netlify/static'));
-router.get('/',(req,res)=>{
-    console.log(' got the request');
-    res.json({ok:1});
-})
+const handler =  async(event, context)=>{
+    const router = express.Router();
+    //router.use(express.static('../functions/netlify/static'));
+    router.get('/',(req,res)=>{
+        console.log(' got the request');
+        res.json({ok:1});
+    })
+    
+    app.use('/api/',router)
+    
+    const serverless = require('serverless-http')
+    
+    return serverless(api)(event, context)    
+}
 
-app.use('/api/',router)
-
-const serverless = require('serverless-http')
-
-module.exports = serverless(app);
+module.exports = {
+    handler
+}
 
