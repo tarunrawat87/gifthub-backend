77086344c5419f9ad50d59d515f011e31035aea0
tarawat <tarawat@paypal.com>
2023-10-02 11:36:13 +0530

    feat: v1

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -6,7 +6,10 @@ const app = express();
 
 const router = express.Router();
 router.use(express.static('../functions/netlify/static'));
-
+app.use((req,res,next)=>{
+    console.log(req.path)
+ next();   
+})
 app.get('/',router)
 
 const serverless = require('serverless-http')
